7f8294baa106348b8ee27fefeb75af35c8d7ac44
Jacob Steringa <jacobsteringa@gmail.com>
2014-07-04 00:52:04 +0200

    Added license

--- Odoo.php
+++ Odoo.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Jacob Steringa <jacobsteringa@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Odoo;
 
 use Zend\XmlRpc\Client as XmlRpcClient;
